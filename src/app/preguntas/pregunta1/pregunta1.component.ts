e6a3b95c0829ddf3d5de097c12706235627bd089
y4zper <78707317+y4zper@users.noreply.github.com>
2024-06-22 21:46:56 -0500

    Pregunta 2 2

--- src/app/preguntas/pregunta1/pregunta1.component.ts
+++ src/app/preguntas/pregunta1/pregunta1.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pregunta1',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pregunta1.component.html',
   styleUrl: './pregunta1.component.css'
 })
 export class Pregunta1Component {
+
+  calcularUtilidad(salario: number, anos: number): number | null {
+    if (isNaN(salario) || isNaN(anos)) {
+      return null;
+    }
+
+    if (anos < 1) {
+      return salario * 0.05;
+    } else if (anos < 2) {
+      return salario * 0.07;
+    } else if (anos < 5) {
+      return salario * 0.10;
+    } else if (anos < 10) {
+      return salario * 0.15;
+    } else {
+      return salario * 0.20;
+    }
+  }
+
+  submit(salarioInput: HTMLInputElement, anosInput: HTMLInputElement): void {
+    const salario = parseFloat(salarioInput.value);
+    const anos = parseInt(anosInput.value, 10);
+    const utilidad = this.calcularUtilidad(salario, anos);
+
+    if (utilidad !== null) {
+      alert('La utilidad calculada es:  ${utilidad.toFixed(2)}');
+    } else {
+      alert('Por favor, ingrese valores validos.');
+    }
+  }
 
 }
